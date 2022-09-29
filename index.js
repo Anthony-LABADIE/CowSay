17d45af0a8e562028ce8cf14a35b94280ed60c6a
ANTHONY LABADIE <anthony.labadie@outlook.fr>
2022-09-29 17:59:10 +0200

    Add cowsay

---
+++ index.js
@@ -0,0 +1,9 @@
+const info = require('./information');
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : (`Hello I'm ${info.name} from ${info.campus}`),
+    e : "oO",
+    T : "U "
+}));
